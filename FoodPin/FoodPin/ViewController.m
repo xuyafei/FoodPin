fe61451acbdbd51ed43f56619eaba4a727e43e5a
xuyafei <xuyafei0208@126.com>
2016-11-15 16:50:01 +0800

    add two restaurant arrays

--- FoodPin/FoodPin/ViewController.m
+++ FoodPin/FoodPin/ViewController.m
@@ -13,6 +13,8 @@
 @property (nonatomic, strong)UITableView *foodRestaurantsTableView;
 @property (nonatomic, copy) NSArray *restaurantArray;
 @property (nonatomic, copy) NSArray *restaurantImages;
+@property (nonatomic, copy) NSArray *restaurantLocations;
+@property (nonatomic, copy) NSArray *restaurantType;
 @end
 
 @implementation ViewController
@@ -27,8 +29,13 @@
 - (void)initRestaurantArray {
     self.restaurantArray = @[@"Cafe Deadend", @"Homei", @"Teakha", @"Cafe Loisl", @"PetiteOyster", @"For Kee Restaurant", @"Po's Atelier", @"Bourke Street Bakery", @"Haigh'sChocolate", @"Palomino Espresso", @"Upstate", @"Traif", @"Graham Avenue Meats AndDeli", @"Waffle & Wolf", @"Five Leaves", @"Cafe Lore", @"Confessional",@"Barrafina", @"Donostia", @"Royal Oak", @"CASK Pub and Kitchen"];
     
-     self.restaurantImages = @[@"cafedeadend.jpg", @"homei.jpg", @"teakha.jpg",@"cafeloisl.jpg", @"petiteoyster.jpg", @"forkeerestaurant.jpg", @"posatelier.jpg",@"bourkestreetbakery.jpg", @"haighschocolate.jpg", @"palominoespresso.jpg",
-         @"upstate.jpg", @"traif.jpg", @"grahamavenuemeats.jpg", @"wafflewolf.jpg",@"fiveleaves.jpg", @"cafelore.jpg", @"confessional.jpg", @"barrafina.jpg",@"donostia.jpg", @"royaloak.jpg", @"thaicafe.jpg"];
+    self.restaurantImages = @[@"cafedeadend.jpg", @"homei.jpg", @"teakha.jpg",@"cafeloisl.jpg", @"petiteoyster.jpg", @"forkeerestaurant.jpg", @"posatelier.jpg",@"bourkestreetbakery.jpg", @"haighschocolate.jpg", @"palominoespresso.jpg",
+        @"upstate.jpg", @"traif.jpg", @"grahamavenuemeats.jpg", @"wafflewolf.jpg",@"fiveleaves.jpg", @"cafelore.jpg", @"confessional.jpg", @"barrafina.jpg",@"donostia.jpg", @"royaloak.jpg", @"thaicafe.jpg"];
+    
+    self.restaurantLocations = @[@"Hong Kong", @"Hong Kong", @"Hong Kong", @"Hong Kong",
+        @"Hong Kong", @"Hong Kong", @"Hong Kong", @"Sydney", @"Sydney", @"Sydney", @"NewYork", @"New York", @"New York", @"New York", @"New York", @"New York", @"New York",@"London", @"London", @"London", @"London"];
+    
+    self.restaurantType = @[@"Coffee & Tea Shop", @"Cafe", @"Tea House", @"Austrian Causual Drink", @"French", @"Bakery", @"Bakery", @"Chocolate", @"Cafe", @"American Seafood", @"American", @"American", @"Breakfast & Brunch", @"Coffee & Tea", @"Coffee& Tea", @"Latin American", @"Spanish", @"Spanish", @"Spanish", @"British", @"Thai"];
 }
 
 - (void)layoutTableView {
@@ -67,8 +74,8 @@
 
     restaurantCell.nameLabel.text = self.restaurantArray[indexPath.row];
     restaurantCell.thumbnailImageView.image = [UIImage imageNamed:self.restaurantImages[indexPath.row]];
-    restaurantCell.locationLabel.text = @"location";
-    restaurantCell.typeLabel.text = @"type";
+    restaurantCell.locationLabel.text = self.restaurantLocations[indexPath.row];
+    restaurantCell.typeLabel.text = self.restaurantType[indexPath.row];
     
     
     return restaurantCell;
