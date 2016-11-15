ef2813f5ebe39faeed42de3d76089bb06bfdea45
xuyafei <xuyafei0208@126.com>
2016-11-15 10:07:18 +0800

    add new imagest.

--- FoodPin/FoodPin/ViewController.m
+++ FoodPin/FoodPin/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController () <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic, strong)UITableView *foodRestaurantsTableView;
 @property (nonatomic, copy) NSArray *restaurantArray;
+@property (nonatomic, copy) NSArray *restaurantImages;
 @end
 
 @implementation ViewController
@@ -24,6 +25,9 @@
 
 - (void)initRestaurantArray {
     self.restaurantArray = @[@"Cafe Deadend", @"Homei", @"Teakha", @"Cafe Loisl", @"PetiteOyster", @"For Kee Restaurant", @"Po's Atelier", @"Bourke Street Bakery", @"Haigh'sChocolate", @"Palomino Espresso", @"Upstate", @"Traif", @"Graham Avenue Meats AndDeli", @"Waffle & Wolf", @"Five Leaves", @"Cafe Lore", @"Confessional",@"Barrafina", @"Donostia", @"Royal Oak", @"CASK Pub and Kitchen"];
+    
+     self.restaurantImages = @[@"cafedeadend.jpg", @"homei.jpg", @"teakha.jpg",@"cafeloisl.jpg", @"petiteoyster.jpg", @"forkeerestaurant.jpg", @"posatelier.jpg",@"bourkestreetbakery.jpg", @"haighschocolate.jpg", @"palominoespresso.jpg",
+         @"upstate.jpg", @"traif.jpg", @"grahamavenuemeats.jpg", @"wafflewolf.jpg",@"fiveleaves.jpg", @"cafelore.jpg", @"confessional.jpg", @"barrafina.jpg",@"donostia.jpg", @"royaloak.jpg", @"thaicafe.jpg"];
 }
 
 - (void)layoutTableView {
@@ -57,7 +61,7 @@
     }
 
     restaurantCell.textLabel.text = self.restaurantArray[indexPath.row];
-    restaurantCell.imageView.image = [UIImage imageNamed:@"restaurant"];
+    restaurantCell.imageView.image = [UIImage imageNamed:self.restaurantImages[indexPath.row]];
     
     return restaurantCell;
 }
